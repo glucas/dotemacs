11a97e3c82983fc08b70d8a571ac5c792cf6ae5e
Greg Lucas <greg@glucas.net>
2018-11-08 15:47:41 -0500

    Initial tweaks
    Disable some unnecessary UI elements.

--- init.el
+++ init.el
@@ -9,6 +9,12 @@
 (message "Starting Emacs %s" emacs-version)
 (add-hook 'after-init-hook (lambda () (message "Initialization complete after %s" (emacs-init-time))) t)
 
+;; minimal UI
+(setq-default visible-bell t)
+(setq inhibit-startup-message t)
+(dolist (mode '(tool-bar-mode scroll-bar-mode))
+  (when (fboundp mode) (funcall mode -1)))
+
 ;; load custom settings
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file nil t)
