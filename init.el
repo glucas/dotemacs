1a8af9c5e97b3946459fb9f1e4a037f1c9d2daff
Greg Lucas <greg@glucas.net>
2019-01-03 19:21:01 -0500

    Hydra to navigate errors

--- init.el
+++ init.el
@@ -430,4 +430,24 @@ _k_: previous    _q_uit
  ("C-c w" . my/clean-buffer)
  ("C-c r" . my/revert-buffer))
 
+;; Navigate errors
+(defhydra hydra-next-error
+  (global-map "C-x")
+  "
+Errors:
+_j_: next error        _h_: first error    _q_uit
+_k_: previous error    _l_: last error
+"
+  ("`" next-error     nil)
+  ("j" next-error     nil :bind nil)
+  ("k" previous-error nil :bind nil)
+  ("h" first-error    nil :bind nil)
+  ("l" (condition-case err
+           (while t
+             (next-error))
+         (user-error nil))
+   nil :bind nil)
+  ("q" nil            nil :color blue))
+
+
 ;;; init.el ends here
